c4a5eca2356efdc8e2fd2daa99f6622ce1eed7cd
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-09-03 16:47:15 -0600

    Added correct stuff.

--- Roberts_Kevin_Assign_2.py
+++ Roberts_Kevin_Assign_2.py
@@ -12,25 +12,20 @@ import matplotlib.pyplot as plt
 #############
 # Problem 1 #
 #############
+print("PROBLEM 1 OUTPUT:")
 
-# NOTE: I'm multiplying some of the rates by 10^{-9} to convert everything to 
-# nanomolars
+# NOTE: I'm multiplying some of the concentrations by 10^{-9} to convert everything to 
+# molars
 
 # defining the rates
 k_cat = 0.1 # (1/s) the caralytic rate
-k_f = (10**5)*(10**(-9)) # (1/(nM*s)) the forward rate constant
-a = 5*(10**4)*(10**(-9)) # (1/(nM*s)) the ratio k_cat/k_m
-k_m = k_cat/a # (nM) since we know that k_cat/k_m = 5*10^4 (1/(M*s))
+k_f = (10**5) # (1/(nM*s)) the forward rate constant
+a = 5*(10**4) # (1/(nM*s)) the ratio k_cat/k_m
+k_m = k_cat/a # (M) since we know that k_cat/k_m = 5*10^4 (1/(M*s))
 k_r = (k_cat*k_f - a*k_cat)/a # (1/s) using the fact that k_m = (k_r+k_cat)/k_f
 
-print(k_r)
-print(k_m)
-print(k_f)
-
-
-
-E_out = 70 # (nM) nanomolars
-S_0 = 200 # (nM) nonmolars
+E_out = 70*(10**(-9)) # (M) nanomolars
+S_0 = 200*(10**(-9)) # (nM) nonmolars
 V_s = 70 # (microliters)
 
 # verifying the (i) condition
@@ -78,16 +73,17 @@ def get_E_o_and_volume(E_o):
         print("Either (i) or (ii) or both was not satisfied.")
         
         
-enzyme_to_test = 22 # (nM)
+enzyme_to_test = 22*(10**(-9)) # (M)
 get_E_o_and_volume(enzyme_to_test)
-
+print()
+print()
 
 
 
 #############
 # Problem 2 #
 #############
-
+print("PROBLEM 2 OUTPUT:")
 # a. The mis-specification of dt is most likely to cause error near the beginning,
 #    when time is close to 0, because that's where the most rapid changes occur.
 # 
@@ -95,21 +91,25 @@ get_E_o_and_volume(enzyme_to_test)
 # 
 # c. To find the rate of change of [ES](t), we must take the derivative of the
 #    equation and evaluate it at 0, which is: 
-#    ans = k_f*E(0)*S(0) - k_r*ES(0) - k_cat*ES(0) -->
-#    ans = 0.0001*22*70 - 0.1*??? - 0.1*??? =  
+#    ans = k_f*(E_0 - ES(0))*S_0 - k_r*ES(0) - k_cat*ES(0) -->
+#    ans = 0.0001*(70 - 0)*200 - 0.1*0 - 0.1*0 = 1.4 (assuming that ES is 0 when 
+#    time t = 0) 
 # 
 # d. 
 # 
 #
-# e.
+# e. Just like the last homework assignment, a reasonable increment in time is 
+# one that does not change the previous value from the new value very much. We 
+# typically want the change to vary from the old to the new value about 10%. So 
+# in this situation we choose the dt to be dt = 0.1 seconds
 # 
 # 
-# f.
+# f. 
 # 
 # 
 # g. 
-
-
+print()
+print()
 
 
 
@@ -121,59 +121,36 @@ get_E_o_and_volume(enzyme_to_test)
 #############
 # Problem 3 #
 #############
-
+print("PROBLEM 3 OUTPUT:")
 
 # defining delta_t and n
 
 # approximating the differential equation with dt = 0.1
 dt = 0.1 # time interval, seconds
-n = int(100/dt)+1 # total intervals
+final_t = 9
+n = int(final_t/dt)+1 # total intervals
 E_0 = enzyme_to_test # (nM) this is the value satisfied by criteria (ii)
-ES_initial = 200 # (nM) (this is an arbitrary value)
-P_initial = 10 # (nM) (this is an arbitrary value)
-S_initial = S_0 # (nM) (this is an arbitrary value)
-
-ES_array = np.zeros((n, 3)) # define array
-P_array = np.zeros((n, 3))
-E_array = np.zeros((n, 3))
-S_array = np.zeros((n, 3))
-
-ES_array[0, 2] = ES_initial # initialize array
-P_array[0, 2] = P_initial
-E_array[0, 2] = E_0
-S_array[0, 2] = S_initial
-
+ES_0 = 0 # (nM) (this is an arbitrary value)
+ES_array = np.zeros((n, 4)) # define array
+ES_array[0, 2] = ES_0 # initialize array
 time = 0
 
 for i in range(1, n):
     ES_array[i, 0] = i
-    P_array[i, 0] = i
-    E_array[i, 0] = i    
-    S_array[i, 0] = i
-    
     time += dt
     ES_array[i, 1] += time
-    P_array[i, 1] += time
-    E_array[i, 1] += time    
-    S_array[i, 1] += time
-    
-    ES_array[i, 2] = dt*(k_f*E_array[i-1, 2]*S_array[i-1, 2] - k_r*ES_array[i-1,2] - k_cat*ES_array[i-1, 2]) + ES_array[i-1, 2]
-    P_array[i, 2] = dt*(k_cat*ES_array[i-1, 2]) + P_array[i-1, 2]
-    E_array[i, 2] = E_0 - ES_array[i-1, 2]
-    S_array[i, 2] = dt*(-k_f*E_array[i-1, 2]*S_array[i-1, 2] + k_r*ES_array[i-1, 2]) + S_array[i-1, 2]
+    ES_array[i, 2] = dt*(k_f*(E_0 - ES_array[i-1,2])*S_0 - k_r*ES_array[i-1,2] - k_cat*ES_array[i-1, 2]) + ES_array[i-1, 2]
 
 
-ES_exact = np.zeros((n, 3))
-def ES_exact_ftn(time_step):
-    return E_0*S_array[i, 2]/(k_m + S_array[i, 2])
+ES_M_M_const = E_0*S_0/(k_m + S_0)
 
+for i in range(0, n):
+    ES_array[i, 3] = ES_M_M_const
 
 x = np.arange(0, n*dt, dt)
 
-plt.plot(x, [row[2] for row in ES_array], label='ES')
-plt.plot(x, [row[2] for row in P_array], label='P')
-plt.plot(x, [row[2] for row in E_array], label='E')
-plt.plot(x, [row[2] for row in S_array], label='S')
+plt.plot(x, [row[2] for row in ES_array], label='ES Finite Difference')
+plt.plot(x, [row[3] for row in ES_array], label='ES Michaelis-Menten')
 
 # Adding a title and labels
 plt.title('Comparing ES finite difference with ES Approximation (3)')
@@ -181,12 +158,12 @@ plt.xlabel('Time in Seconds')
 plt.ylabel('RL')
 
 # adding some scaling
-lower_y = 0
-upper_y = 200
-lower_x = 0
-upper_x = final_t
-plt.ylim(lower_y, upper_y)
-plt.xlim(lower_x, upper_x)
+# lower_y = 0
+# upper_y = np.max([row[2] for row in ES_array])
+# lower_x = 0
+# upper_x = final_t
+# plt.ylim(lower_y, upper_y)
+# plt.xlim(lower_x, upper_x)
 
 
 # Adding a legend to distinguish between the arrays
@@ -195,22 +172,43 @@ plt.legend()
 # Display the plot
 plt.show()
 
+print()
+print()
 
 
 #############
 # Problem 4 #
 #############
+print("PROBLEM 4 OUTPUT:")
+
+# printing out the data to the console
+print("Data from the ES Finite Difference and ES Michaelis-Menten")
+print("j, time, [ES]_Fin_Diff, [ES]_Mich_Ment")
+
+np.set_printoptions(precision=3, threshold=100, edgeitems=5, suppress=False)
+
+print(ES_array)
+print()
+print()
+
 
 
 #############
 # Problem 5 #
 #############
+print("PROBLEM 5 OUTPUT:")
 
+
+t_qssa = k_f*(k_m + S_0)
+
+
+print()
+print()
 
 #############
 # Problem 6 #
 #############
-
+print("PROBLEM 6 OUTPUT:")
 
 
 
