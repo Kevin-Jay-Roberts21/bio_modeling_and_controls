62e8130111d60d7db1157927cc9ea0bac20925e9
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-09-23 16:29:15 -0600

    Finished the first problem.

--- Roberts_Kevin_Assign_5.py
+++ Roberts_Kevin_Assign_5.py
@@ -30,6 +30,68 @@ x_f = 1
 t_0 = 0
 t_f = 0.04
 
+# creating the empty disctretized solution
+n = int(t_f/dt) + 1 # iternation number
+m = int(x_f/dx) + 1
+u = np.zeros((n, m)) # define array 
+
+# populating u with the initial and boundary conditions
+
+for i in range(n):
+    
+    # setting the initial conditions
+    if i == 0:
+        for j in range(m):
+            
+            # setting the boundary conditions
+            if j == 0:
+                u[i, j] = 0
+            elif j == (m-1):
+                u[i, j] = 0
+            else:
+                # setting the boundary conditions
+                if j*dx <= 0.5:     
+                    u[i, j] = 2*j
+                else:
+                    # 2*(1 - x) changes due to the change in dx
+                    u[i, j] = 2*((m-1)-j)
+    else:
+        for j in range(m):
+            
+            # setting the boundary conditions
+            if j == 0:
+                u[i, j] = 0
+            elif j == (m-1):
+                u[i, j] = 0
+            else:
+                u[i, j] = u[i-1,j] + dt/(dx**2)*(u[i-1,j-1] - 2*u[i-1,j] + u[i-1,j+1])
+
+# plotting
+plt.figure()
+x = np.arange(0, m*dx, dx)
+
+print(u[0,:])
+
+t1 = int(0.01/dt)
+t2 = int(0.02/dt)
+t3 = int(0.03/dt)
+t4 = int(0.04/dt)
+
+
+plt.plot(x, u[0,:], label='Temp at t=0')
+plt.plot(x, u[t1,:], label='Temp at t=0.01')
+plt.plot(x, u[t2,:], label='Temp at t=0.02')
+plt.plot(x, u[t3,:], label='Temp at t=0.03')
+plt.plot(x, u[t4,:], label='Temp at t=0.04')
+
+
+# Adding a title and labels
+plt.legend()
+plt.title('Initial Temp')
+plt.xlabel('Space')
+plt.ylabel('Temp')                
+
+
 
 
 
