74a331bc2c8871d2f870fda4a91bd350cb96181d
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-09-28 19:32:19 -0600

    Almost finished the 3rd problem.

--- Roberts_Kevin_Assign_6.py
+++ Roberts_Kevin_Assign_6.py
@@ -308,14 +308,175 @@ plt.ylabel('Percentage Difference')
 # Problem 3 #
 #############
 print()
+print("PROBlEM 3a OUTPUT")
 
 
 # a) Model heat transfer in one dimension for 1 centimeter using equation (1). Maintain
 #    temp. of adjacent tissue at 37+-1C.
 
+# setting some parameters
+dX = 0.0005 # m
+p = 1050 # (kg/m^3)
+Cp = 3770 # J/kg-C
+k = 0.098 # (W/m-C)
+alpha = k/(p*Cp)
+dT_max = (dX**2)/(2*alpha) # in seconds
+dT = round(dT_max, 3) # in seconds
+q_s = -40 # (W/m^2)
+h_c = 250 # (W/m^2-C)
+T_e = 37 # (C)
+Fo = alpha*dT/(dX**2)
+Bi = h_c*dX/k
+print("The following values for alpha, dT_max and dT are: ")
+print("alpha = " + str(round(alpha, 10)))
+print("dT_max = " + str(round(dT_max, 5)))
+print("dT = " + str(dT))
+print()
+print("The diffusivity in this problem is much much smaller than it was in the")
+print("previous problem, which means the tissue diffuses heat much slower than")
+print("metal, which makes sense. This causes dT to be a very large value.")
+print()
+print("The Fourier and Biot numbers are: ")
+print("Fourier: " + str(round(Fo, 3)))
+print("Biot: " + str(round(Bi, 3)))
+
 # b) Calculate U(X, T)
 
+##############################
+# FOR SURFACE FLUX AT BOUNDARY
+
+# changing the dT value as specificed in the problem
+dT = 0.5*dT_max
+
+# space and time intervals
+x_f = 0.01
+
+# creating the empty disctretized solution
+m = int(x_f/dX) + 1
+n = 75
+u = np.zeros((n, m)) # define array 
+
+# setting the initial conditions
+for j in range(m):
+    # setting the boundary conditions
+    if j*dX >= 0.004 and j*dX <= 0.006:
+        u[0, j] = 47
+    else:
+        u[0, j] = 37
+
+
+# plotting
+plt.figure()
+x = np.arange(0, m*dX, dX)
+
+plt.plot(x, u[0,:], 'o', label='Temp at t=0')
+
+# Adding a title and labels
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('Temperature Diffusion of Tissue for Insulation')
+plt.xlabel('Meters')
+plt.ylabel('Temp in C')
+
+
+for i in range(1, n):
+
+    for j in range(m):
+        
+        # setting the boundary conditions
+        if j == 0:
+            u[i, j] = 2*Fo*(u[i-1, 1] + q_s*dX/k) + (1 - 2*Fo)*u[i-1, 1]
+        elif j == (m-1):
+            u[i, j] = 37
+        else:
+            u[i, j] = u[i-1,j] + dT/(dX**2)*alpha*(u[i-1,j-1] - 2*u[i-1,j] + u[i-1,j+1])
+    
+    if i % 15 == 0:
+        plt.plot(x, u[i,:], 'o', label='t={}, u_max = {}'.format(round(i*dT,3), round(max(u[i,:]),3)))
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+u_insulation = u
+
+####################################
+# FOR SURFACE CONVECTION AT BOUNDARY
+
+# creating the empty disctretized solution
+m = int(x_f/dX) + 1
+u = np.zeros((n, m)) # define array 
+
+# setting the initial conditions
+for j in range(m):
+    # setting the boundary conditions
+    if j*dX >= 0.004 and j*dX <= 0.006:
+        u[0, j] = 47
+    else:
+        u[0, j] = 37
+
+
+# plotting
+plt.figure()
+x = np.arange(0, m*dX, dX)
+
+plt.plot(x, u[0,:], 'o', label='Temp at t=0')
+
+# Adding a title and labels
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('Temperature Diffusion of Tissue for Surface Convection')
+plt.xlabel('Meters')
+plt.ylabel('Temp in C')
+
+
+for i in range(1, n):
+
+    for j in range(m):
+        
+        # setting the boundary conditions
+        if j == 0:
+            u[i, j] = 2*Fo*(u[i-1,1] + Bi*T_e) + (1-2*Fo - 2*Fo*Bi)*u[i-1,1]
+        elif j == (m-1):
+            u[i, j] = 37
+        else:
+            u[i, j] = u[i-1,j] + dT/(dX**2)*alpha*(u[i-1,j-1] - 2*u[i-1,j] + u[i-1,j+1])
+    
+    if i % 15 == 0:
+        plt.plot(x, u[i,:], 'o', label='t={}, u_max = {}'.format(round(i*dT,3), round(max(u[i,:]),3)))
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+u_convection = u
+    
 # c) Evaluate your results
 
+# finding the time for when the insulation boundary cond reduces T_max to 40 and 38
 
+insulation_40_t=0
+insulation_38_t=0
+convection_40_t=0
+convection_38_t=0
 
+print(u_convection[n-1,:])
+print(u_insulation[n-1,:])
+
+for k in range(n):
+    if max(u_insulation[k,:]) <= 40:
+        insulation_40_t = i*dT
+        break
+    
+for k in range(n):
+    if max(u_insulation[k,:]) <= 38:
+        insulation_38_t = i*dT
+        break
+    
+for k in range(n):
+    if max(u_convection[k,:]) <= 40:
+        convection_40_t = i*dT
+        break
+    
+for k in range(n):
+    if max(u_convection[k,:]) <= 38:
+        convection_38_t = i*dT
+        break
+print()
+print("PROBLEM 3c OUTPUT")
+print("The time when the insulated boundary cond. reduced to 40C is: " + str(insulation_40_t))
+print("The time when the insulated boundary cond. reduced to 38C is: " + str(insulation_38_t))
+print("The time when the convection boundary cond. reduced to 40C is: " + str(convection_40_t))
+print("The time when the convection boundary cond. reduced to 38C is: " + str(convection_38_t))
