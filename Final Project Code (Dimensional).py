58e7d770d26324e7cec215d8c7bb582928128c50
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-10-22 21:25:47 -0600

    Added the scheme, not working yet.

--- Final Project Code (Dimensional).py
+++ Final Project Code (Dimensional).py
@@ -49,7 +49,7 @@ def B(n):
         return 0
 
 def f(n):
-    return k*c_0*A + k*c_0*B(n)
+    return k*c_0*A # + k*c_0*B(n)
 
 # defining the time and space step as well as total length and total time
 dx = 0.0001 # (m)
@@ -82,37 +82,80 @@ for i in range(n):
     for j in range(m):
         
         
-        if i == 0 and 0 <= j < (L/2)/dx:
+        if i == 0:
             
-            if j == 0: # left boundary conditions
+            # initial conditions
+            if 0 <= j < (L/2)/dx: # (-inf < x < 0) 
                 n_data[i, j] = n_0
                 c_data[i, j] = c_0
-            else: # initial conditions
-                n_data[i, j] = n_0 # (-inf < x < 0)
-                c_data[i, j] = c_0 # (-inf < x < 0)
-        elif i == 0 and (L/2)/dx < j < L/dx:
-            n_data[i, j] = 0
-            c_data[i, j] = 0
-        
-        
-        
-        
-        # the regular discretization
+            else: # (0 <= x < L)
+                n_data[i, j] = 0
+                c_data[i, j] = 0
         else:
-            n_data[i, j] = n_data(i-1, j) + dt*(1/(2*(dx**2))*D_n(c_data[i-1, j])*(n_data[i-1, j+1] - n_data[i-1, j-1]) + s(c_data[i-1, j])*n_data[i-1, j]*(v - n_data[i-1, j]/n_0) - k*n_data[i-1,j])
-            c_data[i, j] = c_data(i-1, j) + dt*(D_c/(dx**2)*(c_data[i-1, j+1] - 2*c_data[i-1, j] + c[i-1, j+1]) + f(n_data(i-1, j)) - mu*c_data[i-1, j]/(c_hat + c_data[j-1, i])*n_data[i-1, j] - delta*c_data[i-1, j])
+            
+            # setting the boundary condition
+            if j == 0: # (x = -inf) 
+                n_data[i, j] = n_0
+                c_data[i, j] = c_0
+            elif j >= m-3: # (neumann boundary conditions)
+                n_data[i, j] = n_data[i, j-2]
+                c_data[i, j] = c_data[i, j-2]
+            else:
+                n_data[i, j] = n_data[i-1, j] + dt*(1/(2*(dx**2))*D_n(c_data[i-1, j])*(n_data[i-1, j+1] - n_data[i-1, j-1]) + s(c_data[i-1, j])*n_data[i-1, j]*(v - n_data[i-1, j]/n_0) - k*n_data[i-1,j])
+                c_data[i, j] = c_data[i-1, j] + dt*(D_c/(dx**2)*(c_data[i-1, j+1] - 2*c_data[i-1, j] + c_data[i-1, j+1]) + f(n_data[i-1, j]) - mu*c_data[i-1, j]/(c_hat + c_data[j-1, i])*n_data[i-1, j] - delta*c_data[i-1, j])
+        
+
+# b) Output the simulation
+# plotting
+plt.figure()
+x = np.arange(0, len(n_data[0,:])*dx, dx)
+
+t1 = int(1/dt)
+t2 = int(5/dt)
+t3 = int(10/dt)
+t4 = int(15/dt)
+t5 = int(20/dt)
+t6 = int(25/dt)
+t7 = int(30/dt)
+t8 = int(35/dt)
+t9 = int(37/dt)
+
+plt.plot(x, n_data[0,:], label='Density at t=0 hours')
+plt.plot(x, n_data[t1,:], label='Density at t=1 hours')
+plt.plot(x, n_data[t2,:], label='Density at t=5 hours')
+plt.plot(x, n_data[t3,:], label='Density at t=10 hours')
+plt.plot(x, n_data[t4,:], label='Density at t=15 hours')
+plt.plot(x, n_data[t5,:], label='Density at t=20 hours')
+plt.plot(x, n_data[t6,:], label='Density at t=25 hours')
+plt.plot(x, n_data[t7,:], label='Density at t=30 hours')
+plt.plot(x, n_data[t8,:], label='Density at t=35 hours')
+plt.plot(x, n_data[t9,:], label='Density at t=37 hours')
+
+# Adding a title and labels
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('Cell Density Spatial Profile')
+plt.xlabel('x axis in m')
+plt.ylabel('Cell Density cell/m^3')
+
+plt.figure()
+x = np.arange(0, len(c_data[0,:])*dx, dx)
+
+plt.plot(x, c_data[0,:], label='EGF at t=0 hours')
+plt.plot(x, c_data[t1,:], label='EGF at t=1 hours')
+plt.plot(x, c_data[t2,:], label='EGF at t=5 hours')
+plt.plot(x, c_data[t3,:], label='EGF at t=10 hours')
+plt.plot(x, c_data[t4,:], label='EGF at t=15 hours')
+plt.plot(x, c_data[t5,:], label='EGF at t=20 hours')
+plt.plot(x, c_data[t6,:], label='EGF at t=25 hours')
+plt.plot(x, c_data[t7,:], label='EGF at t=30 hours')
+plt.plot(x, c_data[t8,:], label='EGF at t=35 hours')
+plt.plot(x, c_data[t9,:], label='EGF at t=37 hours')
+
+# Adding a title and labels
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('EGF Concentration Spatial Profile')
+plt.xlabel('x axis in m')
+plt.ylabel('EGF concentration in mol/m^3')
 
 
 
-
-
-
-
-
-
-
-
-
-
-# defining the EGF concentration and the density
-c_data = np.zeros(())
