5c13f62aa41be54e2c6373fd67f7e430522ccf2f
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-11-15 09:52:49 -0700

    Started the redo.

---
+++ exam_2_redo.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 09:52:17 2024
+
+@author: Kevin Roberts
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 14:39:54 2024
+
+@author: kevinjayroberts
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+# EXAM REDO
+# just redoing the problems that I missed on the exam
+
+
+#############
+# PROBELM 2 #
+#############
+
+#############
+# PROBELM 4 #
+#############
+
+#############
+# PROBELM 6 #
+#############
+
+#############
+# PROBELM 7 #
+#############
+
+#############
+# PROBELM 8 #
+#############
+
+##############
+# PROBELM 11 #
+##############
+
+##############
+# PROBELM 12 #
+##############
